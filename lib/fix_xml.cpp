010259f30c509ccbfed56e2eba86edf72901ad8d
tony zakaria <tonyzakaria53@gmail.com>
2021-07-13 14:14:09 +0200

    adding a funcion to count numebr of tags and their type to be used in error fixing

--- lib/fix_xml.cpp
+++ lib/fix_xml.cpp
@@ -12,3 +12,192 @@ QString fix_xml(QString xml){
     }
     return ans;
 }
+
+
+
+
+
+
+bool check_with_counting(QString file, int* startindex, int* endindex, int* openingtag, int* closingtag, int* selfclosingtag)
+{
+    *openingtag =0 ;
+    *closingtag =0 ;
+    *selfclosingtag = 0;
+
+    QStack<QString> checking;
+    QStack<QString> closing;
+    QStack<int> starting;
+
+    QStack<int> ending;
+
+
+
+
+
+    int len = file.length();
+    for (int i = 0; i < len; i++)
+    {
+        if (file[i] == '<')
+        {
+            if (file[i + 1] == '?')
+            {
+                while (file[i] != '>')
+                {
+                    i++;
+
+                }
+                continue;
+
+            }
+            if (file[i + 1] == '!' && file[i + 2] == '-')
+            {
+                while (file[i] != '>')
+                    i++;
+                continue;
+
+
+
+            }
+            if (file[i + 1] != '/')
+            {
+
+                int start = i + 1;
+                while (file[i] != '>' && file[i] != ' ')
+                {
+                    i++;
+
+
+                }
+                if (file[i] == ' ')
+                {
+                    int end = i;
+                    //                QString tag = file.substr(start, end - start);
+                   QString tag = file.mid(start, end - start);
+                    //                qDebug() << tag;
+                    checking.push(tag);
+                    starting.push(start);
+                    ending.push(end);
+                    *openingtag+=1;
+                    while (file[i] != '>') {
+                        i++;
+
+                    }
+                    if (file[i - 1] == '/')
+                    {
+                        *openingtag-=1;
+                        *selfclosingtag+=1;
+                        checking.pop();
+                        starting.pop();
+                        ending.pop();
+                    }
+
+
+
+                }
+                else
+                {
+
+                    int end = i;
+                    //                QString tag = file.substr(start, end - start);
+                    QString tag = file.mid(start, end - start);
+                    //                qDebug() << tag;
+                    checking.push(tag);
+                    starting.push(start);
+                    ending.push(end);
+                    *openingtag+=1;
+                    if (file[i - 1] == '/')
+                    {
+                        *openingtag-=1;
+                        *selfclosingtag+=1;
+                        checking.pop();
+                        starting.pop();
+                        ending.pop();
+                    }
+
+
+                }
+
+
+            }
+            else
+            {
+                int start = i + 2;
+
+                while (file[i] != '>')
+                {
+                    i++;
+                    if (!(i < len))
+                    {
+                        *startindex = start - 2;
+                        *endindex = len - 1;
+
+                        return false;
+
+                    }
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+                }
+                *closingtag+=1;
+                int end = i;
+                //                QString tag = file.subString(start, end - start);
+                QString tag = file.mid(start, end - start);
+                //                qDebug() << tag;
+                if (!checking.empty() && checking.top() == tag)
+                {
+                    //qDebug().noquote() << checking.top();
+                    checking.pop();
+                    starting.pop();
+                    ending.pop();
+
+                }
+                else
+                {
+                    closing.push(tag);
+
+                    if (*startindex == 0)
+                    {
+                        *startindex = start - 2;
+                        *endindex = end + 1;
+
+                    }
+
+
+                    // return false;
+
+                }
+
+            }
+
+
+
+        }
+
+    }
+    if (closing.empty() && checking.empty())
+    {
+        return true;
+
+    }
+    else
+    {
+        if (closing.empty())
+        {
+            *startindex = starting.top() - 1;
+            *endindex = ending.top() + 1;
+
+
+            return false;
+
+        }
+        else
+        {
+
+            return false;
+        }
+
+    }
+
+
+
+
+}
+
